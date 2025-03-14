360f87e4b7652ac9ff701cefa4ab88002083e909
Suprio-Das <priyamd507@gmail.com>
2025-03-14 14:08:01 +0600

    Back to home set to root

--- src/Components/AddCoffee.jsx
+++ src/Components/AddCoffee.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { HiMiniArrowLeftStartOnRectangle } from "react-icons/hi2";
+import { Link } from 'react-router-dom';
 
 const AddCoffee = () => {
     return (
         <div className='w-[70%] mx-auto my-16'>
-            <h1 className='title text-2xl flex items-center my-3'>
+            <Link className='title text-2xl flex items-center my-3' to='/'>
                 <HiMiniArrowLeftStartOnRectangle /> Back to home
-            </h1>
+            </Link>
             <div className='bg-[#F4F3F0] p-11'>
                 <div className='p-8'>
                     <h1 className='text-3xl title text-center'>Add New Coffee</h1>
