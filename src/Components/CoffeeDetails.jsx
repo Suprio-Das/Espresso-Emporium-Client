5f626bb6e77e315153811f32e048037a44b51546
Suprio-Das <priyamd507@gmail.com>
2025-03-14 14:15:53 +0600

    Static Coffee details component created before backend integration

---
+++ src/Components/CoffeeDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoffeeDetails = () => {
+    return (
+        <div>
+            <h1>This is coffee details page</h1>
+        </div>
+    );
+};
+
+export default CoffeeDetails;
