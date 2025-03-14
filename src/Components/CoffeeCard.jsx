c46b4fec1f6e44f1e6d807c19d636e05231092fc
Suprio-Das <priyamd507@gmail.com>
2025-03-15 01:53:28 +0600

    Delete function implemented with alert

--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -3,7 +3,37 @@ import { IoMdEye } from "react-icons/io";
 import { HiPencil } from "react-icons/hi2";
 import { MdDelete } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 const CoffeeCard = ({ coffee }) => {
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Hey Buddy, Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/coffees/${id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+            }
+        });
+    }
     return (
         <div className='bg-[#F5F4F1] p-2'>
             <div className='flex justify-between items-center'>
@@ -18,7 +48,7 @@ const CoffeeCard = ({ coffee }) => {
                 <div className='flex flex-col justify-center space-y-1 lg:mr-7'>
                     <Link to={`coffees/${coffee._id}`}><p className='bg-[#D2B48C] text-white p-2 w-8 rounded-sm cursor-pointer'><IoMdEye /></p></Link>
                     <Link to={`updateCoffee/${coffee._id}`}><p className='bg-[#3C393B] text-white p-2 w-8 rounded-sm cursor-pointer'><HiPencil /></p></Link>
-                    <p className='bg-[#EA4744] text-white p-2 w-8 rounded-sm cursor-pointer'><MdDelete /></p>
+                    <p onClick={() => handleDelete(coffee._id)} className='bg-[#EA4744] text-white p-2 w-8 rounded-sm cursor-pointer'><MdDelete /></p>
                 </div>
             </div>
         </div>
